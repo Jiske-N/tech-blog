97340f3e260143f4b69ba3c9321d212601d02e43
@r0f33 <jiske.nicolson@gmail.com>
2024-06-12 16:27:28 +0930

    did the same for missed files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,64 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { User } = require('../../models');
+const { User } = require("../../models");
 
 // create new user
-router.post('/', async (req, res) => {
-  try {
-    console.log('userRoutes createNew started');
-    const userData = await User.create(req.body);
-    console.log('userRoutes createNew', userData);
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
+router.post("/", async (req, res) => {
+    try {
+        // add new user to db
+        const userData = await User.create(req.body);
 
-    res.status(200).json(userData);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        // add new user to session
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // existing user login
-router.post('/login', async (req, res) => {
-  try {
-    console.log('userRoutes login started');
-    const userData = await User.findOne({ where: { email: req.body.email } });
-    console.log('userRoutes login', userData);
+router.post("/login", async (req, res) => {
+    try {
+        // locate user in the database
+        const userData = await User.findOne({
+            where: { email: req.body.email },
+        });
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
+        // check they exist
+        if (!userData) {
+            res.status(400).json({
+                message: "Incorrect email or password, please try again",
+            });
+            return;
+        }
+
+        // check password against password in db
+        const validatePassword = await userData.checkPassword(
+            req.body.password
+        );
+
+        // check passwords do match
+        if (!validatePassword) {
+            res.status(400).json({
+                message: "Incorrect email or password, please try again",
+            });
+            return;
+        }
+
+        // update session with newly logged in user
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: "You are now logged in!" });
+        });
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    const validatePassword = await userData.checkPassword(req.body.password);
-
-    if (!validatePassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+// logout user
+router.post("/logout", (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,9 @@ const signupRoutes = require("./user/signupRoutes");
 const dashboardRoutes = require("./dashboard/dashboardRoutes");
 const apiRoutes = require("./api");
 
+// currently have the authorisation on the individual requests within the requisite pages
+// might be better to just place them on the entire routes below
+
 router.use("/", homeRoutes);
 router.use("/comment", homeRoutes);
 router.use("/login", loginRoutes);
